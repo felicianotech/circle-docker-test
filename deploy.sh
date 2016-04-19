4b77a2b2df6ef49af80e47207a98ac460bfb2876
Ricardo N Feliciano <FelicianoTech@gmail.com>
2016-04-19 14:27:27 -0400

    Testing

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 # Exit on any error
 set -e
 
-kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google","image":"us.gcr.io/circle-ctl-test/hello:$CIRCLE_SHA1"}]}}}}'
+kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google","image":"us.gcr.io/circle-ctl-test/hello:`cat commit-hash.txt`"}]}}}}'
