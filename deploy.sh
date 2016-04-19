c57fecd90cf2cc506e9703381cd893a6ab6ea113
Ricardo N Feliciano <FelicianoTech@gmail.com>
2016-04-18 20:31:58 -0400

    Testing

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,4 @@
 # Exit on any error
 set -e
 
-KUBE_CMD=${KUBERNETES_ROOT:-~/kubernetes}/cluster/kubecfg.sh
-
-# Deploy image to private GCS-backed registry
-docker push $EXTERNAL_REGISTRY_ENDPOINT/hello:$CIRCLE_SHA1
-
-# Update Kubernetes replicationController
-envsubst < kubernetes/rails-controller.json.template > rails-controller.json
-$KUBE_CMD -c rails-controller.json \
-    update replicationControllers/railscontroller
-
-# Roll over Kubernetes pods
-$KUBE_CMD rollingupdate railscontroller
+kubectl patch deployment -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google","image":"us.gcr.io/circle-ctl-test/hello:${CIRCLE_SHA1}"}]}}}}'
