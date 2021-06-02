7faefe60a09bdf596c31e649be58d6459da02052
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-06-02 15:19:29 -0400

    Test legacy PG image

---
+++ test.sql
@@ -0,0 +1,6 @@
+CREATE TABLE new_table_name (
+	table_column_title TYPE_OF_DATA column_constraints,
+	next_column_title TYPE_OF_DATA column_constraints,
+	table_constraint
+	table_constraint
+) INHERITS existing_table_to_inherit_from;
